2ad0858b4c30f408866aab1f8fe2ef5686fe2374
Martekode <BrianDesmedt9000@hotmail.com>
2022-08-15 00:47:09 +0200

    styling and logo placement

--- src/Callback.js
+++ src/Callback.js
@@ -1,4 +1,5 @@
 import React , {useState} from "react";
+import logo from "./logo.svg"
 
 const Callback = ({getColor}) => {
     const [activeColor, setActiveColor] = useState("");
@@ -10,7 +11,22 @@ const Callback = ({getColor}) => {
     };
 
     return (
-        <input type="tect" id="input" aria-label="inpur" onChange={handleChange}value={activeColor}/>
+        <div style={{
+            border: "1px solid white",
+            borderRadius: "5px",
+            width: "50%",
+            marginTop: "1em",
+            textAlign: "left",
+        }} >
+        
+        <img alt="lel" style={{
+            height: "1em",
+        }} src={logo}></img>
+            <input style={{
+                width: "90%",
+            }} type="text" id="input" aria-label="inpur" onChange={handleChange}value={activeColor}/>
+        </div>
+        
     )
 }
 export default Callback;
